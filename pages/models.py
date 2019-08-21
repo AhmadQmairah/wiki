318432815af52d605169f7f6fbb5da1d4249f749
Ahmad Qmairah <AhmadQmairah@gmail.com>
2019-08-21 14:41:18 +0300

    Ahmad & Sarah

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.urls import reverse
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
 
 # Create your models here.
+class Page(models.Model):
+	title=models.CharField(max_length=50)
+	content=models.TextField()
+	last_update=models.DateTimeField(auto_now=True)
+
+	def get_absolute_url(self):
+		  
+		  return reverse('page-detail', args=[str(self.id)])
+#@receiver(pre_save, sender=Page)
+#def get_full_name(self, *args, **kwargs):
+#	self.last_update = models.DateField.auto_now()
+    	
+   
+     
+		
